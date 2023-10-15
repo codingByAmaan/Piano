86fe5b86155d6956ac4924905b5c2094b300f10f
amaansheikh <146121940+codingByAmaan@users.noreply.github.com>
2023-10-15 23:19:50 +0500

    Piano

---
+++ app.js
@@ -0,0 +1,59 @@
+let allKeys = document.querySelectorAll("#All-keys li") ;
+let volume = document.getElementById("Volume") ;
+let showKeys = document.getElementById("showKeys");
+let num =  1 ;
+
+let allaudio = [],
+ audio = new Audio('a.wav') ;
+
+let connectKey = (key) => {
+    audio.src = `${key}.wav`;
+    audio.play();
+    let clickKey = document.querySelector(`[data-key="${key}"]`) ;
+    if (clickKey) {
+        clickKey.classList.add("activeKeys");
+        setTimeout(() => {
+            clickKey.classList.remove("activeKeys");          
+        }, 150);
+    }
+}
+
+allKeys.forEach(function(Keys){
+    allaudio.push(Keys.dataset.key) ;
+    Keys.addEventListener("click", () => connectKey(Keys.dataset.key));
+});
+
+let pressedKey =  (e) => {
+        const key = e.key.toUpperCase();
+        if (allaudio.includes(key)) {
+            connectKey(key);
+        }
+}
+
+let  handleVolume = (h) => {
+    audio.volume = h.target.value ;
+}
+
+let keysDisplay = (k) => {
+    let keyWords = document.querySelectorAll("#keyWords") ;
+    if(num === 1){
+        keyWords.forEach((w)=>{
+            w.style.visibility = 'hidden'
+        })           
+    num = 0 ;
+   }else{
+    keyWords.forEach((w)=>{
+        w.style.visibility = "visible"
+    })
+    num = 1 ;
+   }
+}
+
+
+
+document.addEventListener("keydown", pressedKey);
+volume.addEventListener("input", handleVolume) ;
+showKeys.addEventListener("click", keysDisplay) ;
+
+
+
